5736ce643ce9e2772031d98a2821e9071a739e3c
Rafael Cunha <rafael_ptc@hotmail.com>
2018-09-25 18:11:13 -0300

    first commit

---
+++ OnTheMap/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  OnTheMap
+//
+//  Created by Rafael Cunha on 25/09/2018.
+//  Copyright © 2018 Rafael Cunha. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
